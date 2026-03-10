f25f3bbe8fadf5d25e026879d8927b2b941d16f5
Yasmim Kimberly Sampaio <yasmimksdemelo@gmail.com>
2026-03-10 20:44:28 -0300

    Desafio 9

---
+++ Bee1008.py
@@ -0,0 +1,6 @@
+numero = int(input())
+horasTrabalhada = int(input())
+valorRecebido = float(input())
+salario = horasTrabalhada * valorRecebido
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {salario:.2f}")
