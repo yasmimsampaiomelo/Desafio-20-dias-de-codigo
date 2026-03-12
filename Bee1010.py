bbad8b2c136ae4559a30fd1e42115534a91e8a66
Yasmim Kimberly Sampaio <yasmimksdemelo@gmail.com>
2026-03-12 20:58:54 -0300

    Desafio 11

---
+++ Bee1010.py
@@ -0,0 +1,8 @@
+codigoPeca = int(input())
+numeroPeca = int(input())
+valorUnitario = float(input())
+codigoPeca2 = int(input())
+numeroPeca2 = int(input())
+valorUnitario2 = float(input())
+total = numeroPeca * valorUnitario + numeroPeca2 * valorUnitario2
+print(f"VALOR A PAGAR: R$ {total:.2f}")
